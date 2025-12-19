bce722f8087af275e643bbcb1f8bef3e71ff0305
Sanyal Singh <sanyalsingh84@gmail.com>
2025-12-19 10:56:23 +0530

    added fixed for last test case in valid palindrome problem

--- Pattern1-Two_Pointers/Valid-Palindrome.js
+++ Pattern1-Two_Pointers/Valid-Palindrome.js
@@ -14,58 +14,58 @@
  */
 
 // solving with iterative approach
-// var isPalindrome = function (s) {
-//   if (s === "") return true;
-//   let left = 0;
-//   let right = s.length - 1;
+var isPalindrome = function (s) {
+  if (s === "") return true;
+  let left = 0;
+  let right = s.length - 1;
 
-//   while (left < right) {
-//     let leftLetter = s[left].toLowerCase();
-//     let righttLetter = s[right].toLowerCase();
-//     // console.log(leftLetter, righttLetter);
-//     if (leftLetter.charCodeAt(0) < 97) {
-//       left++;
-//       continue;
-//     }
-//     if (righttLetter.charCodeAt(0) < 97) {
-//       right--;
-//       continue;
-//     }
-//     if (leftLetter !== righttLetter) {
-//       return false;
-//     }
-//     left++;
-//     right--;
-//   }
+  while (left < right) {
+    let leftLetter = s[left].toLowerCase();
+    let righttLetter = s[right].toLowerCase();
+    console.log(leftLetter, righttLetter);
+    if (!isAlphaNumeric(leftLetter)) {
+      left++;
+      continue;
+    }
+    if (!isAlphaNumeric(righttLetter)) {
+      right--;
+      continue;
+    }
+    if (leftLetter !== righttLetter) {
+      return false;
+    }
+    left++;
+    right--;
+  }
 
-//   return true;
-// };
+  return true;
+};
 
 // solving with recurssion approach
-var isPalindrome = function (s, left = 0, right = s.length - 1) {
-  s = s.toLowerCase();
-  console.log(s[left], s[right]);
-  // Base case: pointers crossed
-  if (left >= right) return true;
+// var isPalindrome = function (s, left = 0, right = s.length - 1) {
+//   s = s.toLowerCase();
+//   console.log(s[left], s[right]);
+//   // Base case: pointers crossed
+//   if (left >= right) return true;
 
-  // Skip non-alphanumeric from left
-  if (!isAlphaNumeric(s[left])) {
-    return isPalindrome(s, left + 1, right);
-  }
+//   // Skip non-alphanumeric from left
+//   if (!isAlphaNumeric(s[left])) {
+//     return isPalindrome(s, left + 1, right);
+//   }
 
-  // Skip non-alphanumeric from right
-  if (!isAlphaNumeric(s[right])) {
-    return isPalindrome(s, left, right - 1);
-  }
+//   // Skip non-alphanumeric from right
+//   if (!isAlphaNumeric(s[right])) {
+//     return isPalindrome(s, left, right - 1);
+//   }
 
-  // Compare characters (case-insensitive)
-  if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-    return false;
-  }
+//   // Compare characters (case-insensitive)
+//   if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+//     return false;
+//   }
 
-  // Recurse inward
-  return isPalindrome(s, left + 1, right - 1);
-};
+//   // Recurse inward
+//   return isPalindrome(s, left + 1, right - 1);
+// };
 
 function isAlphaNumeric(char) {
   let code = char.charCodeAt(0);
@@ -75,6 +75,7 @@ function isAlphaNumeric(char) {
   );
 }
 
-console.log(isPalindrome("A man, a plan, a canal: Panama"));
+// console.log(isPalindrome("A man, a plan, a canal: Panama"));
 // console.log(isPalindrome("race a car"));
 // console.log(isPalindrome(""));
+// console.log(isPalindrome("0p"));
